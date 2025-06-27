a39f30098f0d410066d552edf60657f1822ffe4a
Gabriel Ehrat <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-06-27 12:41:22 -0300

    feat: Conectando o DB

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,6 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
