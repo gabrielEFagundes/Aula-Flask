43ea22977aac8134ea9d234efb78dfa91af143e0
gabrielEFagundes <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-07-04 12:32:54 -0300

    feat: Página de lista de contatos

--- app/view.py
+++ app/view.py
@@ -1,6 +1,7 @@
 from app import app, db
-from flask import render_template, url_for, request
-from app.forms import ContatoForm
+from flask import render_template, url_for, request, redirect
+from app.forms import ContatoForm   
+from app.models import Contato
 
 @app.route('/')
 def homepage():
@@ -14,18 +15,6 @@ def homepage():
     
     return render_template('index.html', context=context)
 
-'''
-@app.route('/itsok')
-def novapagina():
-    user = 'Gabriel'
-    
-    context = {
-        'user':user
-    }
-    
-    return render_template('itsok.html', context=context)
-'''
-
 @app.route('/contato/', methods=['GET', 'POST'])
 def contato():
     form = ContatoForm()
@@ -33,5 +22,15 @@ def contato():
     
     if form.validate_on_submit():
         form.save()
+        return redirect(url_for('homepage'))
         
     return render_template('contato.html', context=context, form=form)
+
+@app.route('/contato/lista/')
+def contatoLista():
+    dados = Contato.query.all()
+    print(dados)
+    
+    context = {}
+    
+    return render_template('contato_lista.html', context=context)
